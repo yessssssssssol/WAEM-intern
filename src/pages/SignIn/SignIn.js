fd4929ffcb2bec063810e0ffaeb9b5dc834e70c4
Hailey_ys <whassup_man@naver.com>
2023-02-15 09:28:07 +0900

    Add login fetch

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,6 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SignIn = () => {
+  const [loginInfo, setLoginInfo] = useState({
+    loginEmail: '',
+    loginPassword: '',
+  });
+
+  const { loginEmail, loginPassword } = loginInfo;
+  const onClickLogin = (e) => {
+    e.preventDefault();
+    fetch('http://172.20.10.4:3000/user/signin', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json; charset=utf-8' },
+      body: JSON.stringify({
+        email: loginEmail,
+        password: loginPassword,
+      }),
+    })
+      .then((response) => response.json())
+      .then((result) => {
+        console.log(result);
+      });
+  };
+
+  const onHandleLogin = (e) => {
+    const { name, value } = e.target;
+    setLoginInfo((prev) => ({ ...prev, [name]: value }));
+  };
+
   return (
     <div className='font-mono overflow-hidden flex my-24 justify-center'>
       <div className='bg-white lg:w-5/12 md:6/12 w-10/12 shadow-3xl'>
@@ -17,6 +44,8 @@ const SignIn = () => {
             <input
               type='text'
               id='email'
+              name='loginEmail'
+              onChange={onHandleLogin}
               className='bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full'
               placeholder='Email'
             />
@@ -28,12 +57,17 @@ const SignIn = () => {
             <input
               type='password'
               id='password'
+              name='loginPassword'
+              onChange={onHandleLogin}
               className='bg-gray-200 pl-12 py-2 md:py-4 focus:outline-none w-full'
               placeholder='Password'
             />
           </div>
 
-          <button className='bg-[#333333] hover:bg-[#111111] font-medium p-2 md:p-4 text-white uppercase w-full'>
+          <button
+            className='bg-[#333333] hover:bg-[#111111] font-medium p-2 md:p-4 text-white uppercase w-full'
+            onClick={onClickLogin}
+          >
             Login
           </button>
           <a
